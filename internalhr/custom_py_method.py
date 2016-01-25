294c74978564386d73bfe756ae84f509e6cde28d
hatwar <pitambar.hatwar@gmail.com>
2016-01-25 12:12:13 +0530

    permission added in the Employee User list

--- internalhr/custom_py_method.py
+++ internalhr/custom_py_method.py
@@ -1,8 +1,13 @@
+from __future__ import unicode_literals
 import frappe
+from frappe import _
 def notify_leave_Manager(doc,method):
-	manager_list=frappe.db.sql("select cc_name from tabCC where parent = '%s'"%(doc.employee), as_list=1)
-	for email_id in manager_list:
-		subj=_("New Leave Application")
-		messages = ("New leave Application:{0} from Employee:{1}").format(doc.name,doc.employee_name)
-		frappe.sendmail(email_id[0],subject=subj,message=messages)
-		
+	if (not doc.previous_doc) or (doc.previous_doc and \
+				doc.status == "Open" and doc.previous_doc.leave_approver != doc.leave_approver):
+		manager_list=frappe.db.sql("select cc_name from tabCC where parent = '%s'"%(doc.employee), as_list=1)
+		if manager_list:
+			for email_id in manager_list:
+				subj=_("New Leave Application")
+				messages = ("New leave Application:{0} from Employee:{1}").format(doc.name,doc.employee_name)
+				frappe.sendmail(email_id[0],subject=subj,message=messages)
+			
