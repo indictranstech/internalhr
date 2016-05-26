08afdb002b209e48e36394a9e8d3fbb1d294230b
sagar30051991 <sagarshiragawakar@gmail.com>
2016-05-26 12:45:07 +0530

    added the email notigy email message

--- internalhr/custom_py_method.py
+++ internalhr/custom_py_method.py
@@ -8,6 +8,15 @@ def notify_leave_Manager(doc,method):
 		if manager_list:
 			for email_id in manager_list:
 				subj=_("New Leave Application")
-				messages = ("New leave Application:{0} from Employee:{1}").format(doc.name,doc.employee_name)
+				messages = ("New leave Application:{0} from Employee:{1} From:{2} To:{3}").format(doc.name,doc.employee_name,doc.from_date,doc.to_date)
 				frappe.sendmail(email_id[0],subject=subj,message=messages)
-			
+# def notify_leave_approv(doc,method):
+# 	if (not doc.previous_doc) or (doc.previous_doc and \
+# 				doc.status == "Open" and doc.previous_doc.leave_approver != doc.leave_approver):
+# 		manager_list=frappe.db.sql("select leave_approver from `tabLeave Application` where employee_name  = '%s'"%(doc.employee), as_list=1)
+# 		if manager_list:
+# 			for email_id in manager_list:
+# 				subj=_("New Leave Application")
+# 				messages = ("New leave Application:{0} from Employee:{1} From:{2} To:{3}").format(doc.name,doc.employee_name,doc.from_date,doc.to_date)
+# 				frappe.sendmail(email_id[0],subject=subj,message=messages)
+				
